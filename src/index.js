d32a78cd7f35b7f27688ee7a51cf13cc52719fe0
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2018-12-07 09:57:55 +0100

    refactor: moved provider wrapper into index.js from App.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css";
 import { App } from "App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "store";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+import "normalize.css";
+
+ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById("root"));
 registerServiceWorker();
