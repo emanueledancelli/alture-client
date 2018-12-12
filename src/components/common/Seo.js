ca437a39e7c43f43e7e4f68e6b1e24d2f533574e
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2018-12-12 13:10:14 +0100

    add: proptypes

--- src/components/common/Seo.js
+++ src/components/common/Seo.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
 export const Seo = ({ title, description, url, image }) => {
-    return (
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <link rel="canonical" href={url} />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+    </Helmet>
+  );
+};
 
-        <Helmet>
-            <title>{title}</title>
-            <link rel="canonical" href={url} />
-            <meta name="description" content={description} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description}  />
-            <meta property="og:url" content={url} />
-            <meta property="og:image" content={image} />
-        </Helmet>
-    
-    )
+Seo.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
 };
