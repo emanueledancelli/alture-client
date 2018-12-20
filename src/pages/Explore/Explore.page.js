232d4493be5177d2cfb6cae1847bcd59ee386bcc
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2018-12-20 01:00:31 +0100

    add: feedback component

--- src/pages/Explore/Explore.page.js
+++ src/pages/Explore/Explore.page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Seo, Header, Hero, ScrollToTop } from "components/common";
+import "./Explore.page.scss";
 import posed from "react-pose";
 
 const Animated = posed.div({
@@ -26,6 +27,27 @@ export const Explore = () => {
       <Seo />
       <Header close />
       <Hero close hasBorder title="Esplorando i confini" />
+      <p className="explore">
+        Lo spirito dell"esploratore è dentro ciascuno di noi. L"uomo nasce
+        esploratore, il desiderio di scoprire cosa si cela oltre il cancello o
+        dall"altro lato della montagna è una sua tensione naturale. E,
+        nell"immaginario collettivo, l"idea stessa di ricercare luoghi selvaggi
+        e sconosciuti, di spingersi al di fuori della zona di comfort porta
+        subito la mente su una cresta sottile, che sale verso il cielo. La
+        montagna offre ancora molto spazio all"immaginario dell"esplorazione
+        proprio perchè non tutta la sua superficie sembra occupata, e andare
+        verso l"alto diviene quasi metafora dell"anticonformismo, della volontà
+        di non omologarsi: quello di salire è infatti un gesto che sfida la più
+        basica e potente delle leggi naturali, la gravità, e la presenza stessa
+        delle montagne, in fondo, pare una ribellione alla linearità della
+        pianura. Ma la montagna ci parla anche di cose semplici, della terra,
+        dell"acqua, del sole, dell"aria, del nostro essere quegli stessi
+        elementi di cui è composta la natura. Ed ecco che l"esplorazione verso
+        l"altro prosegue, in parallelo, dentro ognuno di noi, alla ricerca di
+        quello che siamo e di quello che sentiamo che ci manca. Perchè
+        esplorare, in fondo, significa sognare di scoprire ciò che possiamo solo
+        immaginare.
+      </p>
     </Animated>
   );
 };
--- src/pages/Feedback/Feedback.page.js
+++ src/pages/Feedback/Feedback.page.js
@@ -1,6 +1,8 @@
 import React from "react";
 import posed from "react-pose";
 import { Seo, ScrollToTop, Header, Hero } from "components/common";
+import { CheckCircleOutlineIcon } from "mdi-react";
+import "./Feedback.page.scss";
 
 const Animated = posed.div({
   enter: {
@@ -19,13 +21,96 @@ const Animated = posed.div({
   }
 });
 
-export const Feedback = () => {
-  return (
-    <Animated>
-      <ScrollToTop />
-      <Seo />
-      <Header close />
-      <Hero close hasBorder title="Feedback" />
-    </Animated>
-  );
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 };
+
+export class Feedback extends React.Component {
+  state = {
+    mail: "",
+    message: "",
+    submitSuccess: false,
+    errorMessage: ""
+  };
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => {
+        this.setState({
+          submitSuccess: true
+        });
+      })
+      .catch(error => {
+        this.setState({
+          errorMessage: error
+        });
+      });
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { email, message, submitSuccess } = this.state;
+    return (
+      <Animated>
+        <ScrollToTop />
+        <Seo />
+        <Header close />
+        <Hero close hasBorder title="Feedback" />
+        <div className="feedback">
+          {submitSuccess ? (
+            <>
+              <span>
+                Il tuo messaggio è stato inviato con successo, grazie!
+              </span>
+              <br />
+              <CheckCircleOutlineIcon size={54} className="feedback__icon" />
+            </>
+          ) : (
+            <>
+              <span>
+                Invia la tua opinione e contribuisci a migliorare alture.org
+              </span>
+              <form onSubmit={this.handleSubmit}>
+                <p>
+                  <label>
+                    <input
+                      type="email"
+                      name="email"
+                      value={email}
+                      placeholder="La tua email"
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <textarea
+                      name="message"
+                      value={message}
+                      placeholder="Inserisci qui il tuo messaggio"
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </p>
+                <p>
+                  <button className="feedback__button" type="submit">
+                    Invia
+                  </button>
+                </p>
+              </form>
+            </>
+          )}
+        </div>
+      </Animated>
+    );
+  }
+}
