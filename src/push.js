514dea5821133cd613f1a36fb199549db57dc3f9
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2019-02-10 16:38:55 +0100

    fix: onesignal

--- src/push.js
+++ src/push.js
@@ -1,13 +1,8 @@
 export const initializeOneSignal = () => {
-  console.log("initializing");
   var OneSignal = window.OneSignal || [];
-  console.log("onesignal:", OneSignal);
-  console.log(OneSignal.push());
   OneSignal.push(function() {
-    console.log(OneSignal.push);
     OneSignal.init({
       appId: "b80e7963-2d68-4e15-ad8d-c79702ee21e6"
     });
-    console.log(OneSignal);
   });
 };
