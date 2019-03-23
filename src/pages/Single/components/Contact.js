21b10754280a52c41e549d1208589f95c945bf07
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2019-03-23 05:54:24 +0100

    chore: fixed media queries

--- src/pages/Single/components/Contact.js
+++ src/pages/Single/components/Contact.js
@@ -4,7 +4,7 @@ import "./Contact.scss";
 export const Contact = mail => (
   <a href={"mailto:" + mail} target="_blank" rel="noopener noreferrer">
     <div className="contact">
-      <div className="contact__container">
+      <div className="contact__text">
         <p>Contatta gli organizzatori</p>
       </div>
     </div>
