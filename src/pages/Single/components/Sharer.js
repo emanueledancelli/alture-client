7f19f213b0e11e1e02d6892522119ff5a3d9b904
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2019-03-23 12:15:29 +0100

    chore: improved sharer css/design

--- src/pages/Single/components/Sharer.js
+++ src/pages/Single/components/Sharer.js
@@ -18,26 +18,22 @@ export const Sharer = props => (
   <Modal>
     <Shade action={props.onClick}>
       <div className="sharer__container">
-        <div>
-          <FacebookShareButton className="sharer__icon" url={props.url}>
-            <FacebookIcon size={40} round />
-            <p>Facebook</p>
-          </FacebookShareButton>
-          <WhatsappShareButton className="sharer__icon" url={props.url}>
-            <WhatsappIcon size={40} round />
-            <p>WhatsApp</p>
-          </WhatsappShareButton>
-        </div>
-        <div>
-          <TelegramShareButton className="sharer__icon" url={props.url}>
-            <TelegramIcon size={40} round />
-            <p>Telegram</p>
-          </TelegramShareButton>
-          <EmailShareButton className="sharer__icon" url={props.url}>
-            <EmailIcon size={40} round />
-            <p>Email</p>
-          </EmailShareButton>
-        </div>
+        <FacebookShareButton className="sharer__icon" url={props.url}>
+          <FacebookIcon size={40} round />
+          <p>Facebook</p>
+        </FacebookShareButton>
+        <WhatsappShareButton className="sharer__icon" url={props.url}>
+          <WhatsappIcon size={40} round />
+          <p>WhatsApp</p>
+        </WhatsappShareButton>
+        <TelegramShareButton className="sharer__icon" url={props.url}>
+          <TelegramIcon size={40} round />
+          <p>Telegram</p>
+        </TelegramShareButton>
+        <EmailShareButton className="sharer__icon" url={props.url}>
+          <EmailIcon size={40} round />
+          <p>Email</p>
+        </EmailShareButton>
       </div>
     </Shade>
   </Modal>
