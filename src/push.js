d472889babb4142b9b64c5b4980f0ad114493a38
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2019-03-24 02:41:40 +0100

    fix: sw

--- src/push.js
+++
@@ -1,8 +0,0 @@
-export const initOneSignal = () => {
-  var OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
-    OneSignal.init({
-      appId: "b80e7963-2d68-4e15-ad8d-c79702ee21e6"
-    });
-  });
-};
