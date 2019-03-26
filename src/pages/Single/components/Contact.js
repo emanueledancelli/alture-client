ee96e6f0dcdf6bb27bbb03c43193d2fd03f13cf3
emanueledancelli@gmail.com <emanueledancelli@gmail.com>
2019-03-26 17:23:00 +0100

    fix: contact button mail bug

--- src/pages/Single/components/Contact.js
+++ src/pages/Single/components/Contact.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Contact.scss";
 
-export const Contact = mail => (
-  <a href={"mailto:" + mail} target="_blank" rel="noopener noreferrer">
-    <div className="contact">
-      <div className="contact__text">
-        <span>Contatta gli organizzatori</span>
+export const Contact = mail => {
+  return (
+    <a href={`mailto:${mail.mail}`} target="_blank" rel="noopener noreferrer">
+      <div className="contact">
+        <div className="contact__text">
+          <span>Contatta gli organizzatori</span>
+        </div>
       </div>
-    </div>
-  </a>
-);
+    </a>
+  );
+};
